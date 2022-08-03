fb111e85f84f7e626ac5eb584c977ba19380d138
Chintalarajesh <109692986+Chintalarajesh@users.noreply.github.com>
2022-08-03 11:10:46 +0530

    first commit

---
+++ src/Testing/tester.java
@@ -0,0 +1,12 @@
+package Testing;
+
+public class tester {
+int a = 10;
+static int b = 20;
+void met1() {
+	int c = 30;
+	System.out.println(c +"is a local variable");
+	System.out.println(a +"is a static variable");
+	System.out.println(b +"is a instants variable");
+}
+}
